e735a4d10fd5e8e20f6109b27f12781dbf67fc8d
maojunzc <maojunzc@example.com>
2026-02-28 18:23:59 +0800

    Add

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import mdx from "@astrojs/mdx"
 import react from "@astrojs/react"
 import sitemap from "@astrojs/sitemap"
+import netlify from "@astrojs/netlify"
 import { pluginCollapsibleSections } from "@expressive-code/plugin-collapsible-sections"
 import { pluginLineNumbers } from "@expressive-code/plugin-line-numbers"
 import tailwindcss from "@tailwindcss/vite"
@@ -9,11 +10,16 @@ import { defineConfig } from "astro/config"
 
 import robotsTxt from "astro-robots-txt"
 
+// 检查是否为生产环境
+const isProduction = process.env.NODE_ENV === 'production'
+
 // https://astro.build/config
 export default defineConfig({
-  output: "static",
+  output: "server",
   prefetch: true,
   site: "https://maojunzc.github.io",
+  // 使用Netlify适配器
+  adapter: netlify(),
   vite: {
     plugins: [tailwindcss()],
   },
