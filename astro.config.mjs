f80586efb12af0cff079ec6d052648e134b08ae4
maojunzc <maojunzc@example.com>
2026-02-28 18:04:49 +0800

    Revert

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import mdx from "@astrojs/mdx"
 import react from "@astrojs/react"
 import sitemap from "@astrojs/sitemap"
-import netlify from "@astrojs/netlify"
 import { pluginCollapsibleSections } from "@expressive-code/plugin-collapsible-sections"
 import { pluginLineNumbers } from "@expressive-code/plugin-line-numbers"
 import tailwindcss from "@tailwindcss/vite"
@@ -10,16 +9,11 @@ import { defineConfig } from "astro/config"
 
 import robotsTxt from "astro-robots-txt"
 
-// 检查是否为生产环境
-const isProduction = process.env.NODE_ENV === 'production'
-
 // https://astro.build/config
 export default defineConfig({
-  output: isProduction ? "server" : "static",
+  output: "static",
   prefetch: true,
   site: "https://maojunzc.github.io",
-  // 生产环境中使用Netlify适配器，开发环境中不使用
-  adapter: isProduction ? netlify() : undefined,
   vite: {
     plugins: [tailwindcss()],
   },
